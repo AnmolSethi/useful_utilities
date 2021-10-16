ff19d25df12f9f80b8f4d4de4274de1a65e32ade
Anmol Sethi <51866009+AnmolSethi@users.noreply.github.com>
2021-10-16 19:49:23 +0530

    Add header for api

--- lib/api/api.service.dart
+++ lib/api/api.service.dart
@@ -3,16 +3,23 @@ part of 'package:useful_utilities/useful_utilities.dart';
 class Api {
   String? body;
 
-  final Map<String, String> headers = {'Content-Type': 'application/json'};
+  final Map<String, String> _headers = {'Content-Type': 'application/json'};
 
   Future callApi(
     String url, {
     required Function(String body) response,
     required Function(Map<String, String> error) catchError,
     String method = 'GET',
+    String? token,
+    Map<String, String> header = const {},
   }) async {
+    final h = {..._headers, ...header};
+    if (token != null) {
+      h.addAll({'Authorization': 'Bearer $token'});
+    }
+
     try {
-      final res = await _parseResponse(url, method.toUpperCase());
+      final res = await _parseResponse(url, method.toUpperCase(), h);
       if (res.statusCode == 200) {
         return response(res.body);
       } else {
@@ -24,7 +31,8 @@ class Api {
     }
   }
 
-  Future<Response> _parseResponse(String url, String method) async {
+  Future<Response> _parseResponse(
+      String url, String method, Map<String, String> headers) async {
     switch (method) {
       case 'GET':
         return get(Uri.parse(url), headers: headers);
