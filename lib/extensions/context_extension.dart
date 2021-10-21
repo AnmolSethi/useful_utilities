3a95a9e8caa892bd9168b1d1be5d3e9ae5f92ee8
Anmol Sethi <51866009+AnmolSethi@users.noreply.github.com>
2021-10-21 19:52:23 +0530

    Function added

--- lib/extensions/context_extension.dart
+++ lib/extensions/context_extension.dart
@@ -3,4 +3,6 @@ part of 'package:useful_utilities/useful_utilities.dart';
 extension ContextExtension on BuildContext {
   double get screenHeight => MediaQuery.of(this).size.height;
   double get screenWidth => MediaQuery.of(this).size.width;
+
+  bool get isDarkMode => Theme.of(this).brightness == Brightness.dark;
 }
